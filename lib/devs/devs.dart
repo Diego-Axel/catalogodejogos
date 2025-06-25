a928c01c548d55cfa9ae5da64dc8044f5b59f15d
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2025-06-25 12:57:16 -0300

    atualizando a página dos desenvolvedores

--- lib/devs/devs.dart
+++ lib/devs/devs.dart
@@ -1,21 +1,45 @@
 import 'package:flutter/material.dart';
 
-class ApiPage extends StatelessWidget {
+class DevsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Informações da API'),
+        title: Text('Desenvolvedores'),
         leading: BackButton(),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Text(
-          'A RAWG é uma das maiores APIs abertas sobre jogos. '
-          'Você pode acessar jogos, desenvolvedores, plataformas e mais. '
-          '\n\nDocumentação oficial: https://api.rawg.io/docs/',
-          style: TextStyle(color: Colors.green, fontSize: 16),
+        child: Column(
+          children: [
+            DevCard(name: 'Diêgo Axel'),
+            SizedBox(height: 16),
+            DevCard(name: 'Samuel Morais'),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class DevCard extends StatelessWidget {
+  final String name;
+
+  const DevCard({required this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.green.shade900,
+      child: ListTile(
+        leading: Container(
+          width: 50,
+          height: 50,
+          color: Colors.black,
+          child: Center(child: Icon(Icons.person, color: Colors.white)),
+        ),
+        title: Text(name, style: TextStyle(color: Colors.white)),
+        subtitle: Text('Foto aqui', style: TextStyle(color: Colors.white60)),
       ),
     );
   }
