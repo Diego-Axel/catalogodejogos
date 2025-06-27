a53841030dd09a09b0fce83d159880c768f14d48
Diêgo Axel Bernardo Santos Rodrigues <diegoaxelbsr@gmail.com>
2025-06-26 22:47:59 -0300

    atualizando a página dos desenvolvedores
    agora, tem o link do giithub dos Devs e pode ser clicado para acessar

--- lib/devs/devs.dart
+++ lib/devs/devs.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DevsPage extends StatelessWidget {
   @override
@@ -12,9 +13,15 @@ class DevsPage extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            DevCard(name: 'Diêgo Axel'),
+            DevCard(
+              name: 'Diêgo Axel',
+              githubUrl: 'https://github.com/Diego-Axel',
+            ),
             SizedBox(height: 16),
-            DevCard(name: 'Samuel Morais'),
+            DevCard(
+              name: 'Samuel Morais',
+              githubUrl: 'https://github.com/SamuelMorais45',
+            ),
           ],
         ),
       ),
@@ -24,8 +31,9 @@ class DevsPage extends StatelessWidget {
 
 class DevCard extends StatelessWidget {
   final String name;
+  final String? githubUrl;
 
-  const DevCard({required this.name});
+  const DevCard({required this.name, this.githubUrl});
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +47,24 @@ class DevCard extends StatelessWidget {
           child: Center(child: Icon(Icons.person, color: Colors.white)),
         ),
         title: Text(name, style: TextStyle(color: Colors.white)),
-        subtitle: Text('Foto aqui', style: TextStyle(color: Colors.white60)),
+        subtitle: githubUrl != null
+            ? InkWell(
+                onTap: () async {
+                  final uri = Uri.parse(githubUrl!);
+                  if (await canLaunchUrl(uri)) { // Verifique se pode lançar a URL
+                    await launchUrl(uri); // Lança a URL no navegador
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Não foi possível abrir o link: $githubUrl')),
+                    );
+                  }
+                },
+                child: Text(
+                  githubUrl!,
+                  style: TextStyle(color: Colors.blueAccent, decoration: TextDecoration.underline),
+                ),
+              )
+            : Text('Informação de contato não disponível', style: TextStyle(color: Colors.white60)),
       ),
     );
   }
